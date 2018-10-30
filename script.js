f7e5dc128a565406962f4230eb8c164660e6dffb
morr1279 <44001975+morr1279@users.noreply.github.com>
2018-10-30 12:41:05 -0500

    save changes

--- script.js
+++ script.js
@@ -10,22 +10,55 @@
 
 // initialize the map
 // We pass the div with id "map" to the L.map function
-var map = L.map('map')
-    // set initial map view to Boston with zoom level 13
-    .setView([39.47, -95.41], 4);
-
+var map = new L.map('map').setView([42.30, -71.58], 10);
+// set initial map view to Boston with zoom level 13
 // load a tile layer
 // loading the base layer of map tiles using a URL template
 // this template ({z}/{x}/{y}) allows leaflet to locate tiles with the
 // correct zoom, x, and y coordinates
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-})
-    // use the .addTo method to add the tile layer to the map.
-    .addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+     }).addTo(map);
+// use the .addTo method to add the tile layer to the map.
+L.geoJson(airports, {
+    
+}).addTo(map);
+
+// Visit http://leaflet-extras.github.io/leaflet-providers/preview/
+// Have fun playing around with different basemaps that are available to use in Leaflet 
 
 // FIXME: Load CSV data into leaflet markers
+// Try Googling "leaflet csv" for a number of options
+// One of the most basic of these is leaflet-omnivore (which handles much more
+// than just CSV files)
+// ref: https://github.com/mapbox/leaflet-omnivore
+// I added csv by converting features in ArcMap to a Json file
 
 // FIXME: Add styling to markers
-// Use airport.png
+// hint: refer to this tutorial for an example of how to do this:
+// ref: https://leafletjs.com/examples/custom-icons/
+// Use airport.png image file
+
+//Add some GeoJSON
+
+var airports = new L.Icon({iconUrl: 'images/marker-icon.png'});
+
+function changesymbols (feature, layer){
+    layer.bindPopup("<h3 class='infoHeader'>Hi, I am an info window</h3><p class='infoHeader'>" + feature.properties.name + "</p>");
+    layer.setIcon(airports);
+};
+
+L.geoJson(airports, {
+    
+   // onEachFeature: changesymbols
+    
+}).addTo(map);
+
+/*var airports = new L.Icon({iconUrl:
+         "http://chittagongit.com//images/airport-icon-png/airport-icon-png-17.jpg"});
+};*/
+
+/*L.geoJson(airports, {
+    
+    onEachFeature: changesymbols
+    
+}).addTo(map);*/
